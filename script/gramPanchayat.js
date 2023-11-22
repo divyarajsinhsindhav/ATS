776646fefed711abe9e560ec45a11e9a5b01e7d2
Vatsal Vala <vatsalvala46@gmail.com>
2023-11-22 16:23:53 +0530

    Added Changes

--- script/gramPanchayat.js
+++ script/gramPanchayat.js
@@ -1,3 +1,45 @@
+        // Get the modal
+        var modal = document.getElementById("myModal");
+        
+        // Get the button that opens the modal
+        var btn = document.getElementById("myBtn");
+        
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+        
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+          modal.style.display = "block";
+        }
+        
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+          modal.style.display = "none";
+        }
+        
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        }
+const nav = document.querySelector('.navbar')
+fetch('../components/navbar1.html')
+.then(res=>res.text())
+.then(data=>{
+    nav.innerHTML=data
+    const parser = new DOMParser()
+    const doc = parser.parserFromString(data, 'text/html')
+})
+
+const footer = document.querySelector('.footer')
+fetch('../components/footer1.html')
+.then(res=>res.text())
+.then(data=>{
+    footer.innerHTML=data
+    const parser = new DOMParser()
+    const doc = parser.parserFromString(data, 'text/html')
+})
 auth.onAuthStateChanged((user) => {
     if (!user) {
         // User is not logged in, redirect to the login page
