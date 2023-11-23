bd6f71881b19d883a969ab3a47a9d6016c20be61
Vatsal Vala <vatsalvala46@gmail.com>
2023-11-23 20:50:37 +0530

    Added latest changes related to UI fixes

--- script/talukaPanchayat.js
+++ script/talukaPanchayat.js
@@ -16,6 +16,7 @@ auth.onAuthStateChanged((user) => {
 
                 // Move the second query inside the loop
                 const application = db.collection('application');
+                let incremental_id = 1;
                 application.where('applicationReceivedBy.Taluka Panchayat', '==', true)
                             .where('taluka', '==', taluka).get().then((snapshot) => {
                     // Handle the results of the second query
@@ -23,7 +24,8 @@ auth.onAuthStateChanged((user) => {
                         console.log(appDoc.id, '=>', appDoc.data());
                         // Process application document as needed
                         // generateButton(appDoc.id, appDoc.data());
-                        addTableRow(appDoc);
+
+                        addTableRow(appDoc, incremental_id++);
                     });
                 });
             });
@@ -31,26 +33,27 @@ auth.onAuthStateChanged((user) => {
     }
 });
 
-const addTableRow = (doc) => {
+const addTableRow = (appDoc, incremental_id) => {
     const tbody = document.getElementById('tbody');
 
     // Create a new 'row' element
     const row = document.createElement('tr');
 
     const srNo = document.createElement('th');
-    srNo.innerText = '2';
+    
+    srNo.innerText = incremental_id ;
     row.appendChild(srNo);
     
     const id = document.createElement('td')
-    id.innerText = `${doc.id}`
+    id.innerText = `${appDoc.id}`
     row.appendChild(id)
     const subject = document.createElement('td');
-    subject.innerText = `${doc.data().subject}`;
+    subject.innerText = `${appDoc.data().subject}`;
     row.appendChild(subject);
 
     const dateTime = document.createElement('td');
     // Assuming ts is the Timestamp object
-    const timestamp = doc.data().timestamp
+    const timestamp = appDoc.data().timestamp
 
     // Convert to milliseconds
     const milliseconds = timestamp.seconds * 1000 + timestamp.nanoseconds / 1e6;
@@ -67,8 +70,8 @@ const addTableRow = (doc) => {
 
     const status = document.createElement('td');
     const badge = document.createElement('label');
-    badge.innerText = doc.data().status.toUpperCase();
-    badge.setAttribute('id', doc.data().status);
+    badge.innerText = appDoc.data().status.toUpperCase();
+    badge.setAttribute('id', appDoc.data().status);
     if (badge.id === 'Accepted' || badge.id === 'Completed') {
         badge.setAttribute('class', 'badge badge-success');
     } else if (badge.id === 'Closed') {
@@ -89,17 +92,17 @@ const addTableRow = (doc) => {
     button.appendChild(buttonRow);
     row.appendChild(button);
 
-    row.setAttribute('id', `tbody ${doc.id}`)
+    row.setAttribute('id', `tbody ${appDoc.id}`)
     tbody.appendChild(row);
 
     button.addEventListener('click', function () {
-        createModal(doc);
+        createModal(appDoc);
       });
 
 };
 
-function createModal(doc) {
-    const timestamp = doc.data().timestamp;
+function createModal(appDoc) {
+    const timestamp = appDoc.data().timestamp;
 
     // Convert to milliseconds
     const milliseconds = timestamp.seconds * 1000 + timestamp.nanoseconds / 1e6;
@@ -112,7 +115,7 @@ function createModal(doc) {
     const istTime = date.toLocaleString('en-US', options);
 
     // Create a new modal
-    const modalId = 'modal-' + doc.id;
+    const modalId = 'modal-' + appDoc.id;
     const modal = document.createElement('div');
     modal.classList.add('modal', 'fade', 'modal-xl');
     modal.id = modalId;
@@ -128,11 +131,11 @@ function createModal(doc) {
                         <div class="row mb-3">
                             <div class="col">
                                 <p class="fw-bold">Application ID:</p>
-                                <p>${doc.id}</p>
+                                <p>${appDoc.id}</p>
                             </div>
                             <div class="col">
                                 <p class="fw-bold">Subject:</p>
-                                <p>${doc.data().subject}</p>
+                                <p>${appDoc.data().subject}</p>
                             </div>
                         </div>
                         <!-- Add more rows for additional details -->
@@ -143,14 +146,14 @@ function createModal(doc) {
                             </div>
                             <div class="col">
                                 <p class="fw-bold">Status:</p>
-                                <p>${doc.data().status}</p>
+                                <p>${appDoc.data().status}</p>
                             </div>
                         </div>
                         <div class="row mb-3">
                             <p class="fw-bold">Document Link:</p>
-                            <p>${doc.data().fileUrl}</p>
+                            <p>${appDoc.data().fileUrl}</p>
                         </div>
-                        <div class="row mb-3" id="button ${doc.id}">
+                        <div class="row mb-3" id="button ${appDoc.id}">
                             <!-- The status update button will be appended here -->
                         </div>
                     </div>
@@ -167,17 +170,17 @@ function createModal(doc) {
     // button.setAttribute('id', 'passApplication');
     button.setAttribute('class', 'btn btn-primary');
     button.textContent = `Pass Application to Jilla Panchayat`;
-    const container = document.getElementById("button " + doc.id);
+    const container = document.getElementById("button " + appDoc.id);
     container.appendChild(button);
 
     // Add a click event listener to the button
     button.addEventListener('click', () => {
         // Handle button click, e.g., show application details
-        console.log(`Button clicked for Application ID: ${doc.id}`);
-        console.log(doc);
+        console.log(`Button clicked for Application ID: ${appDoc.id}`);
+        console.log(appDoc);
 
         // Update the status field in the specific application document
-        const applicationRef = db.collection('application').doc(doc.id);
+        const applicationRef = db.collection('application').appDoc(appDoc.id);
 
         applicationRef.update({
             applicationForwardedTo: {
@@ -194,7 +197,7 @@ function createModal(doc) {
         })
         .then(() => {
             console.log('Application status updated successfully');
-            console.log(doc);
+            console.log(appDoc);
         })
         .catch(error => {
             console.error('Error updating application status:', error);
